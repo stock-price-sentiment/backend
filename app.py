a1f926066ca404d62f057ef8c89df8572fa6c955
Eric <eric_gomez_1@hotmail.com>
2019-07-12 19:27:50 -0700

    added the postgres db connection which still needs to be refactored, will work on that next

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from decouple import config
+
+USER = 'postgres'
+PW = 'password'
+URL = '172.26.0.3:5432'
+DB = 'stock'
+
+DATABASE_URI = f"postgresql+psycopg2://{USER}:{PW}@{URL}/{DB}"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def sanitationCheck():
-    return jsonify({'message': 'Hello World.'})
+class Count(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Count id={self.id} value={self.count}>"
+
+
+db.create_all()
+
+count = Count(count=0)
+db.session.add(count)
+db.session.commit()
+
+
+@app.route('/', defaults={'name': 'John Doe'})
+@app.route('/<name>')
+def sanitationCheck(name):
+    return jsonify({'message': f"Hello, {name}"})
+
+
+@app.route('/hello')
+def hello():
+    global count
+    count.count += 1
+    db.session.commit()
+    return jsonify({'count': count.count})
