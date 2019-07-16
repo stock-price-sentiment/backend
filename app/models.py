777ba8df04b7e9ade3d3340fc20584b19d3c6fad
Eric <eric_gomez_1@hotmail.com>
2019-07-16 01:37:40 -0700

    had to move the user_stock def from the bottom to top due to relationship references

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
 from app import db
 
+user_stock = db.Table('user_stock', 
+  db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+  db.Column('stock_id', db.Integer, db.ForeignKey('stock.id'), primary_key=True)
+)
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(100), unique=True, nullable=False)
@@ -22,8 +27,3 @@ class Value(db.Model):
   created = db.Column(db.DateTime, nullable=False)
   price = db.Column(db.Numeric(precision=8, scale=2), nullable=False)
   stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'), nullable=False)
-
-user_stock = db.Table('user_stock', 
-  db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-  db.Column('stock_id', db.Integer, db.ForeignKey('stock.id'), primary_key=True)
-)
