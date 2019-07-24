d9f22f0c062409a494557426d70c480690c777a3
Eric <eric_gomez_1@hotmail.com>
2019-07-24 10:54:57 -0700

    restructured the tests dir, started testing the model properties

---
+++ tests/test_models.py
@@ -0,0 +1,26 @@
+import unittest, nose2
+from nose2.tools import params
+from app.models import User
+
+class TestUser(unittest.TestCase):
+  user = None
+
+  def setUp(self):
+    self.user = User()
+
+  def test_id(self):
+    ID = 1
+
+    self.user.id = ID
+    
+    self.assertEqual(self.user.id, ID)
+    self.assertEqual(type(self.user.id), int)
+
+  def test_email(self):
+    EMAIL = 'test@email.com'
+
+    self.user.email = EMAIL
+
+    self.assertEqual(self.user.email, EMAIL)
+    self.assertEqual(type(self.user.email), str)
+
