f6fd487d99bdd46816d172314b6816506c875951
Eric <eric_gomez_1@hotmail.com>
2019-08-17 13:44:35 -0700

    added error handling to both save and get all stocks

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -1,13 +1,20 @@
 from app import db
 from app.models import Stock
+from flask import abort
 
 class StockService():
   def save_stock(self, stock):
-    db.session.add(stock)
-    db.session.commit()
+    try:
+      db.session.add(stock)
+      db.session.commit()
+    except:
+      abort(400, 'Could not save stock to database, please make sure attributes are correct and try again.')
   
   def get_all_stocks(self):
-    return Stock.query.all()
+    try:
+      return Stock.query.all()
+    except:
+      abort(500, 'Could not retrieve stock data set from database. Try again later.')
   
   def get_stock_by_id(self, id):
     return Stock.query.get_or_404(id, description=f'No Stock Found By ID {id}')
